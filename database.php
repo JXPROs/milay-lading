cafc1d977def82f801285e2ad146d5da00bb8529
Fetra <jx.propitech@gmail.com>
2025-06-06 22:11:24 +0300

    Update database.php

--- database.php
+++ database.php
@@ -1,1 +1,20 @@
+<?php
+// Paramètres de connexion
+$host = 'mysql-milay.alwaysdata.net';
+$dbname = 'milay_db';
+$username = 'milay';
+$password = '@@Mamimamy00@@';
 
+try {
+    // Création d'une instance PDO
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+
+    // Configuration des attributs PDO
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Connexion réussie à la base de données.";
+} catch (PDOException $e) {
+    // En cas d'erreur
+    echo "Erreur de connexion : " . $e->getMessage();
+}
+?>
