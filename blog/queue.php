17a565337b6a97fcec92e9138e6b49ee97d88a69
Fetra <jx.propitech@gmail.com>
2025-06-20 21:37:51 +0300

    Update queue.php

--- blog/queue.php
+++ blog/queue.php
@@ -1,1 +1,15 @@
 
+<?php
+$to = "jx.propitech@gmail.com";
+$subject = "Message important - Ne pas répondre";
+$message = "Bonjour,\n\nCeci est un message automatique. Merci de ne pas répondre.";
+$headers = "From: Ne pas répondre <milay@alwaysdata.net>\r\n";
+$headers .= "Reply-To: milay@alwaysdata.net\r\n";
+$headers .= "X-Mailer: PHP/" . phpversion();
+
+if (mail($to, $subject, $message, $headers)) {
+    echo "Email envoyé avec succès.";
+} else {
+    echo "Échec de l'envoi de l'email.";
+}
+?>
